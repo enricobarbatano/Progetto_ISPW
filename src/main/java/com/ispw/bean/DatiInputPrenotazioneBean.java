a3d8258cb0796b5266f56dba81cde468267eac45
enrico barbatano <enrybarba2002@gmail.com>
2026-02-06 16:38:41 +0100

    risoluzione bug e code smell

--- src/main/java/com/ispw/bean/DatiInputPrenotazioneBean.java
+++ src/main/java/com/ispw/bean/DatiInputPrenotazioneBean.java
@@ -2,14 +2,4 @@
 package com.ispw.bean;
 
 public class DatiInputPrenotazioneBean extends BasePrenotazioneBean {
-
-    private String oraFine;
-
-    public String getOraFine() {
-        return oraFine;
-    }
-
-    public void setOraFine(String oraFine) {
-        this.oraFine = oraFine;
-    }
 }
